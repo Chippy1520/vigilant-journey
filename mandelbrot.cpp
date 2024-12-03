3ec2bef000232f5b1b747dee44547dac56cadd5d
Chathuka Elapatha <93859394+Chippy1520@users.noreply.github.com>
2024-12-03 21:20:36 +0530

    Update mandelbrot.cpp

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -7,12 +7,42 @@
 int WIDTH = 1000;
 int HEIGHT = 1000;
 
-float magnitude(sf::Vector2f vec)
+
+
+static float magnitude(sf::Vector2f vec)
 {
     float x = vec.x;
     float y = vec.y;
 
     return (sqrt(x * x + y * y));
+}
+
+sf::RectangleShape calc_point(int x_cord, int y_cord)
+{
+    float a = x_cord / 500.0 * 2;
+    float b = y_cord / 500.0 * 2;
+    int n = 0;
+    float x = a;
+    float y = b;
+
+
+    sf::Vector2f z = { 0.f,0.f };
+
+    while (magnitude(z) < 2 && n <= 255)
+    {
+        z = { x,y };
+        float temp_x = x * x - y * y + a;
+        float temp_y = 2 * x * y + b;
+        x = temp_x;
+        y = temp_y;
+        n++;
+    }
+
+    sf::RectangleShape pointer;
+    pointer.setFillColor(sf::Color(n, n, n));
+    pointer.setSize({ 1.f,1.f });
+    pointer.setPosition((float)x_cord + WIDTH / 2.0, (float)y_cord + HEIGHT / 2.0);
+    return pointer;
 }
 
 
@@ -22,50 +52,17 @@ int main()
 
     sf::Clock clock;
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Insert Text Here");
-    window.setFramerateLimit(1);
+    window.setFramerateLimit(60);
 
     sf::RenderTexture texture;
-    texture.create(WIDTH,HEIGHT);
-    
-
-    for(int x_cord = -500;x_cord < 500; x_cord++)
-    {
-        for(int y_cord = -500;y_cord < 500; y_cord++)
-        {
-            float a = x_cord / 500.0 * 2;
-            float b = y_cord / 500.0 * 2;
-            int n = 0;
-            float x = a;
-            float y = b;
-
-
-            sf::Vector2f z = {0.f,0.f};
-
-            while(magnitude(z) < 2 && n <= 255)
-            {
-                z = {x,y};
-                float temp_x = x * x - y * y + a ;
-                float temp_y = 2 * x * y + b;
-                x = temp_x;
-                y = temp_y;
-                n++;
-            }
-            
-            sf::RectangleShape pointer;
-            pointer.setFillColor(sf::Color(n,n,n));
-            pointer.setSize({1.f,1.f});
-            pointer.setPosition((float)x_cord + WIDTH / 2.0 , (float)y_cord + HEIGHT / 2.0 );
-            texture.draw(pointer);
+    texture.create(WIDTH, HEIGHT);
+    int x_cord = -500;
+    int y_cord = -500;
 
 
 
 
 
-        }
-    }
-
-    
-    
 
 
     while (window.isOpen())
@@ -81,10 +78,21 @@ int main()
                 window.close();
             }
         }
+        
+        
+        if (x_cord <= 500) 
+        {
+            for (y_cord = -500; y_cord < 500; y_cord++)
+            {
+                texture.draw(calc_point(x_cord, y_cord));
+            }
 
-        window.clear(sf::Color::Black);
+        }
+        
 
-
+        x_cord++;
+        //std::cout << x_cord << " , " << y_cord << "\n";
+        window.clear();
         texture.display();
         sf::Sprite sprite(texture.getTexture());
         window.draw(sprite);
@@ -93,9 +101,3 @@ int main()
     }
     return 0;
 }
-
-
-
-
-
-
